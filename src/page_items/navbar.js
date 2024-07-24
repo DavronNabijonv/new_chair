7efe4a99e17f4e24ee78e1774860ac7b9f810b9c
Elyor BGRMD <you@example.combato3616524@gmail.com>
2024-07-24 07:41:38 +0500

    innirWidth

--- src/page_items/navbar.js
+++ src/page_items/navbar.js
@@ -8,7 +8,7 @@ import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { SlHome } from "react-icons/sl";
 
 function Navbar() {
-  const [innerWidth, setInnerWidth] = useState(1500);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [scrollDownf, setScrollDownf] = useState(false);
 
   useEffect(() => {
@@ -30,6 +30,8 @@ function Navbar() {
       window.removeEventListener('scroll', handleDown);
     };
   }, []);
+
+
 
   return (
     <>
