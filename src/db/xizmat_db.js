6b34d4cb3712d6a4f149b730d42f8a4ab8d9ab2b
Elyor BGRMD <you@example.combato3616524@gmail.com>
2024-07-27 18:35:04 +0500

    xizmat_db information

---
+++ src/db/xizmat_db.js
@@ -0,0 +1,65 @@
+import { SiAntdesign } from "react-icons/si";
+import { GrTechnology } from "react-icons/gr";
+import { MdOutlineHighQuality,MdOutlineAddHomeWork,MdOutlineMoreTime,MdPayment } from "react-icons/md";
+import { TbRulerMeasure,TbTruckDelivery } from "react-icons/tb";
+import { BsWrenchAdjustable } from "react-icons/bs";
+import { RiContractLine,RiCustomerService2Line } from "react-icons/ri";
+
+
+export const xizmat = [
+    {
+        nameServices:"Interier design",
+        icon: <SiAntdesign />,
+        textServices:"Interior design orqali biz sizning ehtiyojlaringiz va didingizga mos keladigan chiroyli va funktsional dizaynni yaratishga yordam beramiz. Biz sizning uyingizda qulay va uyg'un joy yaratish uchun mavjud materiallar va mebellardan foydalanamiz. Bizning jamoamiz sizga loyihada yordam berishga va ishning eng yuqori sifatini ta'minlashga doimo tayyor. Uyingizni interyerini o`z xohishingizday yarating . Bunda biz zamonaviy dasturlardan foydalangan holda amalga oshiramiz."
+    },
+    {
+        nameServices:"Yuqori aniqlikdagi texnikalardan foydalanish",
+        icon: <GrTechnology />,
+        textServices:" Hozirgi kunda texnologiyalarsiz tassovur qilib bo’lmaganidek SerenMebel sizga zamonoviy va yuqori aniqlikda ishlaydigan texnikalar va asboblardan foydalangan holda buyurtmangizni tayyorlab berishadi , zamonoviy texnikalar orqali biz aniq o’lchamdagi va sifatli bo’lgan mebel tayyorlash imkoniyatiga ega bolamiz . Bunda bizga Xitoy , Turkiya va Rassiya davlatlaridan olib kelingan texnika va asboblar yordam beradi."
+    },
+    {
+        nameServices:"Yuqori sifatli materiallar",
+        icon: <MdOutlineHighQuality />,
+        textServices:"Mebellarimiz albatta sifatli materiallardan tayyorlanadi har bir detalga alohida e’tibor qaratiladi. Foydalaniladigan materiallarni siz tanlash imkoniyatiga ega bolasiz , bunda sizga material rangi , qalinligi , ishlab chiqarilgan mamlakati haqida ma’lumot beriladi. Mdf , Dsp , Akril , L-mdf , L-dsp Mdf 1 yoki 2 taraflama laminatsiyalangan Turkiya , Xitoy , Rassiya davlatlarida ishlab chiqarilgan bo’yoqlar ."
+    },
+    {
+        nameServices:"O`lchash ishlari",
+        icon: <TbRulerMeasure />,
+        textServices:"Bu bilan siz o’z uyingizga mos o’lchamdagi mebelga ega bo’lasiz. Mutahasislarimiz sizning uyingizdan kelib chiqan holda sizga maslahatlarini berishadi . O’lchash ishlari siz buyurtma berishdan oldin qilinadigan ish hisoblanadi . O’lchash ishlarini bizning ko’p yillik tajribaga ega mutahasislarimiz amalga oshirishadi va o’z maslahatlarini aytishadi ."
+    },
+    {
+        nameServices:"Yetkazib berish",
+        icon: <TbTruckDelivery />,
+        textServices:"Bizning jamoamiz sizni vaqtingizni tejaydi va sizga qulayliklar yaratadi . Biz shahar bo’ylab sizning buyurtmangizni sizning manzilingizgacha xavfsiz va sifatiga zarar yetmagan holda yetkazib berishda sizga yordam berishadi . Buyurmangizni siz uydan chiqmagan holda qabul qilib olish imkoniyatlarini yaratadi ."
+    },
+    {
+        nameServices:"O`rnatish ishlari",
+        icon: <MdOutlineAddHomeWork />,
+        textServices:"Tayyor bo’lgan mebel yaxshi o’rnatilmasa vaqt o’tgan sayin noqulayliklar keltirib chiqarmasligi uchun bizning yuqori malakali mutahasislarimiz sizning mebelingizni sifatli qilib o’rnatib berishadi , sifatli o’rnatilgan mebeldan esa bemalol qo’rqmasdan foydalana olasiz . O’rnatish ishlari sizning buyurtmangiz tayyor bo’lgandan so’ng amalga oshiriladi ."
+    },
+    {
+        nameServices:"Texnik yordam",
+        icon: <BsWrenchAdjustable />,
+        textServices:"SerenMebel sizga qulayliklar yaratishda davom etadi . Siz buyurtmangizni qabul qilgandan so’ng 3 oy davomida bizga murojat qilib texnik yozdam so’rashingiz mumkin . Bunda sizga biz mutahasislarimizni yuborib noqulayliklarni bartaraf etishda yordam berishadi. Mijozlarmizga yordam berishdan mamnunmiz."
+    },
+    {
+        nameServices:"Buyurtmani o`z vaqtida bajarish",
+        icon: <MdOutlineMoreTime />,
+        textServices:"Albatta buyurtma berilgandan so’ng uni o’z vaqtida tayyor bo’lish ham muhim bo’lganligi uchun biz sizlarga buyurtmangizni o’z vaqtida tayyor bolishini kafolatlaymiz. Vaqt hammamiz uchun muhim."
+    },
+    {
+        nameServices:"Ikki tomonlama shartnoma",
+        icon: <RiContractLine />,
+        textServices:"Buyurtmangiz ikki tomonlaman shartnoma asosida amalga oshiriladi . Bunda sizga shartnoma beriladi va bir nusxasi bizda qoladi. Bu shartnomada buyurtmangiz qanchada tayyor bolish vaqti , kelishilagan narx , sizning buyurtmangiz haqida ma’lumot va shunga o’xshash narsalar kiritilgan bo’ladi."
+    },
+    {
+        nameServices:"To`lov turlari",
+        icon: <MdPayment />,
+        textServices:"To’lov turlari sizning imkoniyatingiz va hohshingizdan kelib chiqqan holda amalga oshiriladi, Naqt pul , plastik kartadan to’lov, hisob raqdan pul o’tkazish yo’li, payme, click, uzum bank va shunga o’xshash ilovalar orqali ham to’lov amalga oshirish imkoniyatlarini ham biz sizga taqdim etamiz"
+    },
+    {
+        nameServices:"Maslahatlar",
+        icon: <RiCustomerService2Line />,
+        textServices:"Uyingizga qanday mebel mos kelishini bilmasdan ikkilanyabsizmi ? Bunda sizga bizning mutahasislarimiz yaqindan yordam berishadi , sizning didingizga va uyingizga mos tushadigan mebellarni tanlashda o’z maslahatlarini berishadi."
+    },
+]
