31092a6b7bb50df5cd447c5f5f1e1c7006fc6128
Elyor BGRMD <you@example.combato3616524@gmail.com>
2024-07-28 09:13:31 +0500

    about_page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import React, { useCallback, useEffect, useState, useContext } from "react";
 import AsosiySwiper from "./mainPartsOfPage/swiper_slider/asosiy";
 import PastkiSlider from "./mainPartsOfPage/swiper_slider/b_of_asosiy";
 import Product from "./mainPartsOfPage/swiper_slider/product";
+import About from "./pages/page_items/about";
 import Navbar from "./pages/page_items/navbar";
 import Xizmat from "./pages/page_items/xizmat";
 
+export const InnerWidthContext = React.createContext()
+
 function App() {
+  const [innerWidth, setInnerWidth] = useState(
+    window.innerWidth
+  );
+
+  const handleResize = useCallback(() => {
+    setInnerWidth(window.innerWidth);
+  }, [innerWidth]);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="App">
-      <Navbar />
+      <InnerWidthContext.Provider value={innerWidth}>
+        <Navbar />
       <AsosiySwiper/>
       <PastkiSlider/>
       <Product/>
       <Xizmat/>
+        <About />
+      </InnerWidthContext.Provider>
     </div>
   );
 }
