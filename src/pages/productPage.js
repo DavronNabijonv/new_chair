275b55d2f6cc2e779429096b07f08b6fd4838add
Elyor BGRMD <you@example.combato3616524@gmail.com>
2024-08-01 19:03:13 +0500

    scrol_to _top_0

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./productPage.css";
 import Product_info from "./product_page_items/product_info";
 import Product_bottom from "./product_page_items/product_bottom";
 
 export default function ProductPage() {
+  useEffect(()=>{window.scrollTo(0, 0);},[])
   return (
     <div className="productHome">
       <div className="product_page">
