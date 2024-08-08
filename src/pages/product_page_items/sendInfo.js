94bdcc4fbddc7c9a8f1d50f20e27a2cdc053a96e
Elyor BGRMD <you@example.combato3616524@gmail.com>
2024-08-08 18:51:35 +0500

    sendInfo_product_part

--- src/pages/product_page_items/sendInfo.js
+++ src/pages/product_page_items/sendInfo.js
@@ -6,21 +6,25 @@ export default function SendInfo() {
     <div className="send_info">
       <div className="s_header">
         <h2>Ma'lumotlar:</h2>
-        <button>Buyurtma berish</button>
+        <button className="sendInfo_btn">Buyurtma berish</button>
       </div>
       <div className="get_info">
         <div className="user_info">
-            <label>Ism Familya:</label>
+          <label>Ism Familya:</label>
           <input type="text" placeholder="Ismingiz" required />
           <label>Telefon raqam:</label>
           <input type="text" placeholder="Telefon raqam" required />
           <label>Buyurtmachi manzili:</label>
           <input type="text" placeholder="Manzil" required />
+          <label>Qo`shimcha takliflar</label>
           <textarea placeholder="Qo`shimcha takliflar!!!" />
         </div>
         <div className="user_info">
+          <label>Mahsulot nomi:</label>
           <input type="text" placeholder="Mahsulot nomi" />
-          <input type="text" placeholder="Narxi" disabled />
+          <label >Narxi:</label>
+          <input type="text" value={'1 200 000'} placeholder="Narxi" disabled />
+          <label htmlFor="">Soni:</label>
           <input type="text" placeholder="Soni"  />
         </div>
       </div>
