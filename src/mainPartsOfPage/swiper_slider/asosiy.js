5018ede9eae782c68ece18d860a9ba4adaec9b12
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-08-23 21:10:30 +0500

    mobile_slider

--- src/mainPartsOfPage/swiper_slider/asosiy.js
+++ src/mainPartsOfPage/swiper_slider/asosiy.js
@@ -1,10 +1,10 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/swiper-bundle.css"; // Main Swiper styles
-import "swiper/css/pagination"; // Additional styles for pagination, if needed
-import "swiper/css/navigation"; // Additional styles for navigation, if needed
+import "swiper/swiper-bundle.css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
@@ -14,34 +14,30 @@ import { images_info } from "../../db/showCase_slider";
 import { InnerWidthContext } from "../../App";
 
 export default function AsosiySwiper() {
-
   const innerWidthAsosiySwiper = useContext(InnerWidthContext);
-  
 
   return (
-    <>
-      <div className="asosiy">
-        <Swiper
-          spaceBetween={0}
-          centeredSlides={true}
-          pagination={true}
-          loop={true}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          navigation={true}
-          slidesPerView={innerWidthAsosiySwiper>670?1:1.5}
-          modules={[Pagination, Navigation, Autoplay]}
-          className="mySwiper"
-        >
-          {images_info.map((r) => (
-            <SwiperSlide key={r.img_url}>
-              <img src={`${r.img_url}`} className="asosiy_slider_img" alt="Slide Image" />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </>
+    <div className="asosiy">
+      <Swiper
+        spaceBetween={innerWidthAsosiySwiper > 370 ? 8 : 8} // Adjust for 25% visibility on mobile
+        centeredSlides={true}
+        pagination={true}
+        loop={true}
+        autoplay={{
+          delay: innerWidthAsosiySwiper> 370? 3000 : 1500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        slidesPerView={innerWidthAsosiySwiper > 670 ? 1 : 1.2} // 1.5 to show 25% of the adjacent slides
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
+      >
+        {images_info.map((r) => (
+          <SwiperSlide key={r.img_url}>
+            <img src={r.img_url} className="asosiy_slider_img" alt="Slide Image" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React  from 'react';
 import AsosiySwiper from '../mainPartsOfPage/swiper_slider/asosiy';
 import PastkiSlider from '../mainPartsOfPage/swiper_slider/b_of_asosiy';
 import Product from '../mainPartsOfPage/swiper_slider/product';
@@ -7,20 +7,12 @@ import About from './home_page_items/about';
 import Tarmoq from './home_page_items/tarmoq';
 import Aloqa from './home_page_items/aloqa';
 import { Element } from 'react-scroll';
-import { InnerWidthContext } from '../App';
-import Mobile_as from '../mainPartsOfPage/swiper_slider/mobile_as';
 
 export default function HomePage() {
-  const { mainInnerWidth } = useContext(InnerWidthContext);
-
-  // Debug: Log the mainInnerWidth value to ensure it's correct
-  useEffect(() => {
-    console.log('Screen width:', mainInnerWidth);
-  }, [mainInnerWidth]);
 
   return (
     <div>
-      {mainInnerWidth < 425 ? <Mobile_as /> : <AsosiySwiper />}
+      <AsosiySwiper/>
 
       <PastkiSlider />
 
